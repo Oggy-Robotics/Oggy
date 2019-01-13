840b358357d052c7ed27e00d6ced27012d4196aa
drahonov.p@gmail.com <drahonov.p@gmail.com>
2019-01-13 09:38:59 +0100

    Aktualizace buildings _ advanced

--- Ogame Robot/Clases/Buildings.cs
+++ Ogame Robot/Clases/Buildings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,61 +18,109 @@ namespace Ogame_Robot.Clases
 
         public void Inicialization()
         {
-             
+
         }
 
 
 
 
 
-        public void AutomatickaStavba(BrowserManipulation browser)
+        public TimeSpan AutomatickaStavba(BrowserManipulation browse, int vychozíRezim)
         {
-            this.browserManipulation = browser;
-            List<Player.Planet> PlanetaHrace = InfoDoly();
+            List<Player.Planet> PlanetaHrace = new List<Player.Planet>();
+            List<string> cas_dalsiho_volani = new List<string>();
+
+            /*       Režim                  */
             List<int> rezim = new List<int>();
-            for (int i2 = 0; i2 < PlanetaHrace.Count(); i2++)
+            for (int i2 = 0; i2 < browserManipulation.InfoPlayerNumberOfPlanets(); i2++)
             {
-                rezim.Add(1);
+                rezim.Add(vychozíRezim);   //výchozí režim je 0 => nedělá nic
+                PlanetaHrace.Add(new Player.Planet());
             }
+                ; //možnost vybrat stavební režim pro planety
+            //rezim[4] = 2; rezim[6] = 2;
+            /*  Nastavuje 5. a 7. planetu do režimu 2     */
 
-            ;
-            for (int a = 0; a < PlanetaHrace.Count(); a++)
+
+
+            for (int a = 0; a < browserManipulation.InfoPlayerNumberOfPlanets(); a++)
             {
-
+                //Pro planety v režimu 0 ani nebudem hledat info :D
+                if (rezim[a] != 0)
+                {
+                    PlanetaHrace[a].suply = browserManipulation.InfoSuply(a + 1);   //získání infa o dolech atd na planetě 1->X
+                }
                 switch (rezim[a])
                 {
+                    /*     čas = -1            */
+                    case 0: {/* Recess -> nestaví */ cas_dalsiho_volani.Add("-1"); } break;
 
-                    case 1:
-                        /*
-                        Balanced
-                        Snaží se mít kov,krystal,S.elektrárnu na stejné urovni 
-                        když dul na kov je nižší než krystal
-                        */
+                    case 1:/* Balanced 
+                        Snaží se mít kov a krystal na stejné urovni 
+                        když dul na kov je nižší než nebo roven krystalu, vylepší ho, jinak vylepší krystal*/
                         {
-                            browser.UpgradeSuply(1);
-                            /*if (PlanetaHrace[a].suply[0].lv < PlanetaHrace[a].suply[1].lv)
+                            //                  kov    je menší nebo roven     krystal
+                            if (PlanetaHrace[a].suply[0].lv <= PlanetaHrace[a].suply[1].lv)
                             {
-                                //něco se možná stane
-                                
-                            }*/
-                        }
-                        break;
-                    /*
-                    ProductionBased
-                    Snaží se vyrovnávat produkci (kov je vyšší)
+                                try
+                                {
+                                    //browserManipulation.ChangePlanet(a + 1);  //not needed if used within search for informations
+                                    browserManipulation.UpgradeSuply(1);    // 1=kov
 
 
-
-                    */
-                    case 2:
-                        {
-                            if (PlanetaHrace[a].suply[0].lv < PlanetaHrace[a].suply[1].lv)
+                                    cas_dalsiho_volani.Add(browse.WaitForElement(By.XPath("/html[1]/body[1]/div[2]/div[2]/div[1]/div[3]/div[2]/div[4]/div[2]/ul[1]/li[2]/div[1]/div[1]/div[1]/a[1]")).Text);
+                                }
+                                catch (Exception)
+                                {
+                                    cas_dalsiho_volani.Add(browse.WaitForElement(By.XPath("/html[1]/body[1]/div[2]/div[2]/div[1]/div[3]/div[2]/div[4]/div[2]/ul[1]/li[2]/div[1]/div[1]/div[1]/a[1]")).Text);
+                                }
+                            }
+                            //                  kov    je větší než           krystal 
+                            if (PlanetaHrace[a].suply[0].lv > PlanetaHrace[a].suply[1].lv)
                             {
-                                //něco se možná stane
+                                //browserManipulation.ChangePlanet(a + 1);    //not needed if used within search for informations
+                                browserManipulation.UpgradeSuply(2);    // 2=krystal
+                                ;
                             }
                         }
                         break;
-                    case 3:
+
+                    case 2:/* Balanced+Solar
+                        Snaží se mít kov,krystal,S.elektrárnu na stejné urovni 
+                        když dul na kov je nižší než krystal
+                        Elektrárna > Kov > krystal  */
+                        {
+                            //                  elek    je menší nebo roven     kovu     a nebo              elek    je menší nebo roven     krystalu
+                            if ((PlanetaHrace[a].suply[4].lv <= PlanetaHrace[a].suply[0].lv) | (PlanetaHrace[a].suply[4].lv <= PlanetaHrace[a].suply[1].lv))
+                            { /* pak se vylepší elektrárna */
+                                //browserManipulation.ChangePlanet(a + 1);  //not needed if used within search for informations
+                                browserManipulation.UpgradeSuply(4);    //elektrárna
+                                ;
+                            }
+                            else /* když je ale elektrárna top minimálně o 1lvl nad nejlepším z dolů (kov/krystal)
+                                    pak staví důl, kov má přednost stejně jako v case 1 */
+                            {
+                                //                  kov    je menší nebo roven     krystal
+                                if (PlanetaHrace[a].suply[0].lv <= PlanetaHrace[a].suply[1].lv)
+                                {
+                                    //browserManipulation.ChangePlanet(a + 1);  //not needed if used within search for informations
+                                    browserManipulation.UpgradeSuply(1);    // kov
+                                    ;
+                                }
+                                //                  kov    je větší než           krystal 
+                                if (PlanetaHrace[a].suply[0].lv > PlanetaHrace[a].suply[1].lv)
+                                {
+                                    //browserManipulation.ChangePlanet(a + 1);  //not needed if used within search for informations  
+                                    browserManipulation.UpgradeSuply(2);    //krystal
+                                    ;
+                                }
+                            }
+
+                        }
+                        break;
+
+                    case 3:/* ProductionBased
+                        Snaží se vyrovnávat produkci (kov je vyšší) */
                         {
                             if (PlanetaHrace[a].suply[0].lv < PlanetaHrace[a].suply[1].lv)
                             {
@@ -89,27 +138,93 @@ namespace Ogame_Robot.Clases
                             }
                         }
                         break;
-                    default:
+                }//end switch
+            }//end for
 
+
+            List<TimeSpan> cas = new List<TimeSpan>();
+            TimeSpan min_cas = new TimeSpan();
+            for(int i = 0; i < cas_dalsiho_volani.Count; i++)
+            {
+                cas.Add(ConvertTextToTimespan(cas_dalsiho_volani[i]));
+
+                //if(cas_dalsiho_volani[i]=="-1") { cas_dalsiho_volani[i]="blablabla" }
+            }
+            min_cas=cas.Min();
+            if(min_cas==TimeSpan.Zero) { min_cas = TimeSpan.FromSeconds(-1); }
+            return min_cas;
+        }
+
+        public TimeSpan ConvertTextToTimespan(string text)
+        {
+            TimeSpan time = new TimeSpan();
+            //1d 35min
+            string[] texts = text.Split(' ');
+            List<int> numbers = new List<int>();
+            List<string> stringy = new List<string>();
+
+            for (int b = 0; b < texts.Count(); b++)
+            {
+                int pocet = 0;
+                for (int i = 0; i < texts[b].Length; i++)
+                {
+                    int a = -1;
+                    try
+                    {
+                        a = Convert.ToInt32(texts[b][i]);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                    if (a <= 57 && a >= 48)
+                    {
+                        pocet++;
+                    }
+
+                }
+                int a2 = 0;
+                int.TryParse(texts[b].Substring(0, pocet), out a2);
+                numbers.Add(a2);
+
+                stringy.Add(texts[b].Substring(pocet, texts[b].Length - pocet));
+
+            }
+
+            for (int i = 0; i < stringy.Count; i++)
+            {
+                switch (stringy[i])
+                {
+                    case "t":
+                        {
+                            time = time.Add(TimeSpan.FromDays(7 * numbers[i]));
+                        }
                         break;
+                    case "d":
+                        {
+                            time = time.Add(TimeSpan.FromDays(numbers[i]));
+                        }
+                        break;
+                    case "hod":
+                        {
+                            time = time.Add(TimeSpan.FromHours(numbers[i]));
+                        }
+                        break;
+                    case "min":
+                        {
+                            time = time.Add(TimeSpan.FromMinutes(numbers[i]));
+                        }
+                        break;
+                    case "s":
+                        {
+                            time = time.Add(TimeSpan.FromSeconds(numbers[i]));
+                        }
+                        break;
+
                 }
             }
+            return time;
         }
 
-
-        public List<Player.Planet> InfoDoly()
-        {
-
-            List<Player.Planet> planet = new List<Player.Planet>();
-            for (int i = 0; i < browserManipulation.InfoPlayerNumberOfPlanets(); i++)
-            {
-                planet.Add(new Player.Planet());
-
-                planet[i].suply = browserManipulation.InfoSuply(i + 1);
-
-            }
-            return planet;
-        }
 
     }
 }

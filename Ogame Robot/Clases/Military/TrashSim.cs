93656bb5ccc602d50d5254ddd1758fe30b6e801f
tomas drahonovsky <tomasdrah@seznam.cz>
2019-01-13 09:07:10 +0100

    fixed cleaning text

--- Ogame Robot/Clases/Military/TrashSim.cs
+++ Ogame Robot/Clases/Military/TrashSim.cs
@@ -43,41 +43,64 @@ namespace Ogame_Robot.Clases
 
 
             //research
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackerTechArmour), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathAtackerTechArmour), TrashDriver).SendKeys(Convert.ToString(myResearch[(int)ResearchInfo.battleArmor]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackerTechShield), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathAtackerTechShield), TrashDriver).SendKeys(Convert.ToString(myResearch[(int)ResearchInfo.battleShields]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackerTechWeapons), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathAtackerTechWeapons), TrashDriver).SendKeys(Convert.ToString(myResearch[(int)ResearchInfo.battleWeapons]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackerTechChemicalCombusion), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathAtackerTechChemicalCombusion), TrashDriver).SendKeys(Convert.ToString(myResearch[(int)ResearchInfo.driveChemical]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackerTechImpulse), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathAtackerTechImpulse), TrashDriver).SendKeys(Convert.ToString(myResearch[(int)ResearchInfo.driveImpulse]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackerTechHypersace), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathAtackerTechHypersace), TrashDriver).SendKeys(Convert.ToString(myResearch[(int)ResearchInfo.driveHyper]));
 
             //attacker fleet
-                WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "202")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.SmallCargo]));
-                WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "203")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.LargeCargo]));
-                WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "204")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.LightFighter]));
-                WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "205")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.HeavyFighter]));
-                WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "206")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.Cruiser]));
-                WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "207")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.Battleship]));
-                WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "208")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.ColonyShip]));
-                WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "209")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.Recycler]));
-                WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "210")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.EspProbe]));
-                WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "211")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.Bomber]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "202")), TrashDriver).Clear();
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "202")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.SmallCargo]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "203")), TrashDriver).Clear();
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "203")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.LargeCargo]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "204")), TrashDriver).Clear();
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "204")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.LightFighter]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "205")), TrashDriver).Clear();
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "205")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.HeavyFighter]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "206")), TrashDriver).Clear();
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "206")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.Cruiser]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "207")), TrashDriver).Clear();
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "207")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.Battleship]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "208")), TrashDriver).Clear();
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "208")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.ColonyShip]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "209")), TrashDriver).Clear();
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "209")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.Recycler]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "210")), TrashDriver).Clear();
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "210")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.EspProbe]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "211")), TrashDriver).Clear();
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "211")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.Bomber]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "213")), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "213")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.Destroyer]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "214")), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "214")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.Deathstar]));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "215")), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathAtackersFleet.Replace("&", "215")), TrashDriver).SendKeys(Convert.ToString(myfleet[(int)UnitType.Battlecruiser]));
 
             //attacker cord
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackerPositionGalaxi), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathAtackerPositionGalaxi), TrashDriver).SendKeys(Convert.ToString(coordinates.galaxy));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackerPositionSystem), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathAtackerPositionSystem), TrashDriver).SendKeys(Convert.ToString(coordinates.system));
+            WaitForElement(By.XPath(TrashSimLib.relxpathAtackerPositionPosition), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathAtackerPositionPosition), TrashDriver).SendKeys(Convert.ToString(coordinates.planet));
 
             //API
+            WaitForElement(By.XPath(TrashSimLib.relxpathdefendersAPI), TrashDriver).Clear();
             WaitForElement(By.XPath(TrashSimLib.relxpathdefendersAPI), TrashDriver).SendKeys(API);
             WaitForElement(By.XPath(TrashSimLib.relxpathLoadSpyMessageButton), TrashDriver).Click();
             Thread.Sleep(50);
 
             //Simulate
             bool unloaded = true;
-            while(unloaded)
+            while (unloaded)
             {
                 try
                 {

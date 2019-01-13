1ddca20846585a5e6bbf770cb8f80588aac770a9
drahonov.p@gmail.com <drahonov.p@gmail.com>
2019-01-13 11:13:25 +0100

    Fix

--- Ogame Robot/Clases/AutoBuilder.cs
+++ Ogame Robot/Clases/AutoBuilder.cs
@@ -47,33 +47,6 @@ namespace Ogame_Robot.Clases
         }
 
 
-        public class FarmInactive_Short : PackedFunction
-        {
-            public BrowserManipulation browser;
-            public int numberOfCall = 1;
-
-            public FarmInactive_Short(Timer timer, BrowserManipulation browser, TimeSpan standardFrequency)
-            {
-                this.browser = browser;
-                this.standardFrequency = standardFrequency;
-                timer.packedFunctions.Add(this);
-            }
-            public FarmInactive_Short(Timer timer, BrowserManipulation browser, TimeSpan standardFrequency, TimeSpan addTime)
-            {
-                this.browser = browser;
-                nextCall = DateTime.Now + addTime;
-                this.standardFrequency = standardFrequency;
-                timer.packedFunctions.Add(this);
-            }
-            public override TimeSpan CallFunction()
-            {
-                browser.FarmInactive(1, 200,0.05, true, 15, 3, 450, true);
-
-
-                numberOfCall++;
-                return standardFrequency;
-            }
-        }
 
 
     }
